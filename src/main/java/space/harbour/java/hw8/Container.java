2604744437dc28283c605708a7ea2696d3ea4999
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-19 11:08:00 +0700

    Add the giveMeMoney() method

---
+++ src/main/java/space/harbour/java/hw8/Container.java
@@ -0,0 +1,54 @@
+package space.harbour.java.hw8;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Container {
+    private int denomination;
+    private int count;
+    private Container nextInChain;
+
+    public Container(int denomination, int count) {
+        // Size of bills: 1, 5, 10, 20, 50 euros
+        this.denomination = denomination;
+        // Number of bills in this dispenser
+        this.count = count;
+    }
+
+    public void setNextInChain(Container container) {
+        nextInChain = container;
+    }
+
+    public Map<Integer, Integer> tryGiveMoney(int amount, Map<Integer, Integer> allBillsGiven) {
+        // Use Chain of Responsibility to hand out the cash
+        // https://java-design-patterns.com/patterns/chain/
+        //return cash.handle(amount);
+        int billsGiven = Math.min(amount / denomination, count);
+
+        count -= billsGiven;
+        amount -= billsGiven * denomination;
+        if (allBillsGiven.containsKey(denomination)) {
+            allBillsGiven.put(denomination, allBillsGiven.get(denomination) + billsGiven);
+        } else {
+            allBillsGiven.put(denomination, billsGiven);
+        }
+
+        if (nextInChain != null) {
+            nextInChain.tryGiveMoney(amount, allBillsGiven);
+        } else {
+            if (amount > 0) {
+                System.out.println("Sorry we are currently out of money."
+                        + "Please try a smaller amount or another ATM nearby.");
+            } else {
+                System.out.println("Please take your card and your bills.");
+                System.out.print("Here are your bills: ");
+                System.out.println(allBillsGiven);
+            }
+        }
+
+        return allBillsGiven;
+    }
+
+
+
+}
---
+++ src/test/java/space/harbour/java/hw8/AtmTest.java
@@ -0,0 +1,23 @@
+package space.harbour.java.hw8;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AtmTest {
+
+    // Test if cash = null, then Balance = 0
+    // and giveMeMoney still works, but rejects all requests
+    @Test
+    public void testNull() {
+        System.out.print("Allgood");
+    }
+
+}
