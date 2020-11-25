292f5f2a29f771b6fa3826a4454ae03c7d1e014f
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-25 09:43:11 +0700

    Finish review of the class

--- src/main/java/space/harbour/java/hw12/courseexamples/MiniWebsite.java
+++ src/main/java/space/harbour/java/hw12/courseexamples/MiniWebsite.java
@@ -1,11 +1,23 @@
 package space.harbour.java.hw12.courseexamples;
 
+
+import static spark.Spark.get;
+import static spark.Spark.halt;
+
 import com.google.gson.Gson;
 import spark.Spark;
 
-import static spark.Spark.get;
+
 
 public class MiniWebsite {
+    public static class Message {
+        public String message;
+
+        public Message(String message) {
+            this.message = message;
+        }
+    }
+
     public static void main(String[] args) {
         get("/hello", (request, response) -> "Hello World");
 
@@ -17,9 +29,27 @@ public class MiniWebsite {
 
         Gson gson = new Gson();
         Spark.get("/message", (request, response) -> new Message("Hello GSON"), gson::toJson);
+        //same thing, old style:
         Spark.get("/json", (request, response) -> new Gson().toJson(new Message("Hello JSON")));
 
-        
+        get("/private", ((request, response) -> {
+            response.status(401);
+            return "Go Away!";
+        }));
+
+        get("/protected", ((request, response) -> {
+            halt(403);
+            return null;
+        }));
+
+        get("/redirect", (request, response) -> {
+            response.redirect("/sum/1024/2048");
+            return null;
+        });
+
+
+
+
 
 
 
